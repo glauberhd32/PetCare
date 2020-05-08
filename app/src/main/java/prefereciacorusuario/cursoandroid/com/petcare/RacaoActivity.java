d8eb806fd02bc4bcd84efe73db7aa711817cf94d
glauberhd32 <glauberhd32@gmail.com>
2020-05-08 11:05:16 -0300

    PetCare V2 tela Escolha de Produtos petCareV3 raçoesTela petcareV4 raçoes  peixe tela

--- app/src/main/java/prefereciacorusuario/cursoandroid/com/petcare/RacaoActivity.java
+++ app/src/main/java/prefereciacorusuario/cursoandroid/com/petcare/RacaoActivity.java
@@ -1,25 +1,24 @@
 package prefereciacorusuario.cursoandroid.com.petcare;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class EscolhaProdutoActivity extends AppCompatActivity {
-    private Button botaoRacao;
+public class RacaoActivity extends AppCompatActivity {
+   private  Button racaoDEPeixe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_escolha_produto);
+        setContentView(R.layout.activity_racao);
 
-        botaoRacao = (Button)findViewById(R.id.botaoRacaId);
+        racaoDEPeixe = (Button)findViewById(R.id.botao_racao_peixe);
 
-        botaoRacao.setOnClickListener(new View.OnClickListener() {
+        racaoDEPeixe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(EscolhaProdutoActivity.this,RacaoActivity.class));
+
             }
         });
     }
---
+++ app/src/main/java/prefereciacorusuario/cursoandroid/com/petcare/RacaoPeixeActivity.java
@@ -0,0 +1,13 @@
+package prefereciacorusuario.cursoandroid.com.petcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RacaoPeixeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_racao_peixe);
+    }
+}
