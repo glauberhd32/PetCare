ad52a37b9cdebb8ab02d3d08765a40fea4bddcf2
glauberhd32 <glauberhd32@gmail.com>
2020-05-08 10:33:41 -0300

    PetCare V2 tela Escolha de Produtos petCareV3

---
+++ app/src/main/java/prefereciacorusuario/cursoandroid/com/petcare/EscolhaProdutoActivity.java
@@ -0,0 +1,26 @@
+package prefereciacorusuario.cursoandroid.com.petcare;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class EscolhaProdutoActivity extends AppCompatActivity {
+    private Button botaoRacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escolha_produto);
+
+        botaoRacao = (Button)findViewById(R.id.botaoRacaId);
+
+        botaoRacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(EscolhaProdutoActivity.this,RacaoActivity.class));
+            }
+        });
+    }
+}
