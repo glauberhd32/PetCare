88cbb13ebf808ee7247d83230b82907fed5f82e0
glauberhd32 <glauberhd32@gmail.com>
2020-05-22 14:48:52 -0300

    telas tela  medicamento tela acessorio tela comedouro e bebedouro

--- app/src/main/java/prefereciacorusuario/cursoandroid/com/petcare/AcessorioActivity.java
+++ app/src/main/java/prefereciacorusuario/cursoandroid/com/petcare/AcessorioActivity.java
@@ -1,13 +1,26 @@
 package prefereciacorusuario.cursoandroid.com.petcare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AcessorioActivity extends AppCompatActivity {
+    private Button botaoComedouro_Bebedouro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acessorio);
+
+        botaoComedouro_Bebedouro = (Button)findViewById(R.id.botaoBedouroComedouro);
+
+        botaoComedouro_Bebedouro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AcessorioActivity.this,BebedouroEcomedouroActivity.class));
+            }
+        });
     }
 }
